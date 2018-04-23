9fae5cda2eaf4e1bc86c2114ea1816e94a139be3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/B143073/Form1.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace B143073 {
+    public partial class Form1 : XtraForm {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // TODO: This line of code loads data into the 'nwindDataSet.Products' table. You can move, or remove it, as needed.
+            this.productsTableAdapter.Fill(this.nwindDataSet.Products);
+            myGridView1.ActiveFilterString = "CategoryID = 1";
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to implement your own Filter Builder dialog for the GridView
+
+
+<p>This example demonstrates how to add an additional functionality to the standard Filter Builder dialog. For example, disable the "OK" and "Apply" buttons until the user modifies the filter criteria.</p>
+
+
+<h3>Description</h3>
+
+<p>Starting with version 2011 vol 2, DevExpress WinForms controls no longer support Visual Studio 2005 and .NET Framework 2.0 and 3.0: <a href="http://www.devexpress.com/Support/WhatsNew/DXperience/files/11.2.5.bc.xml#BC-FW"><u>BC-FW: For WinForms and ASP.NET Controls</u></a>. </p>
+
+<br/>
+
+
